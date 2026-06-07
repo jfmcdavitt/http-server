43a32d8ed0dabff7dd044a665d0b7b6697e3c335
jfmcdavitt <mcdavittjacob@gmail.com>
2026-06-07 17:27:20 -0400

    Test from OS

--- server.c
+++ server.c
@@ -1,57 +1,54 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /* Error Reporting */
-void error(char *msg)
-{
+void error(char *msg) {
   perror(msg);
   exit(1);
 }
 
-/* Main function for main server functionality */
-int main(int argc, char *argv[]) 
-{
-  /* 
-  *  -- Declaring -- 
-  *
-  *  sockfd, newsockfd: the file descriptors for the sockets
-  *  portno: the port number for the server 
-  *  clilen: The size of the address of the client
-  *  n: Return value for read and write system calls
-  *
-  */
+/* Main function for main servers functionality */
+int main(int argc, char *argv[]) {
+  /*
+   *  -- Declaring --
+   *
+   *  sockfd, newsockfd: the file descriptors for the sockets
+   *  portno: the port number for the server
+   *  clilen: The size of the address of the client
+   *  n: Return value for read and write system calls
+   *
+   */
   int sockfd, newsockfd, portno, clilen, n;
-  
+
   /* Buffer to store read messages from the client */
   char buffer[256];
 
-  /* Structure for storing internet addresses */ 
+  /* Structure for storing internet addresses */
   struct sockaddr_in serv_addr, cli_addr;
-  
+
   /* Make sure a port is provided when setting up the server */
-  if (argc < 2)
-  {
+  if (argc < 2) {
     fprintf(stderr, "ERROR, no port provided\n");
     exit(1);
   }
 
-  /* Set up Server Socket File Descriptor */ 
+  /* Set up Server Socket File Descriptor */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   /* If the value returned is less than 0, there was an error */
   if (sockfd < 0) {
     error("ERROR opening socket\n");
   }
-  
-  /* Initialize the server address struct to zeros */ 
-  bzero((char *) &serv_addr, sizeof(serv_addr));
 
-  /* get the port number supplied by the command line */ 
+  /* Initialize the server address struct to zeros */
+  bzero((char *)&serv_addr, sizeof(serv_addr));
+
+  /* get the port number supplied by the command line */
   portno = atoi(argv[1]);
 
   serv_addr.sin_family = AF_INET;
@@ -60,17 +57,16 @@ int main(int argc, char *argv[])
 
   serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-  {
+  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     error("ERROR on binding\n");
   }
 
   listen(sockfd, 5);
 
   clilen = sizeof(cli_addr);
-  
-  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-  
+
+  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
   if (newsockfd < 0) {
     error("ERROR on accept\n");
   }
@@ -79,9 +75,8 @@ int main(int argc, char *argv[])
 
   n = read(newsockfd, buffer, 255);
 
-  if (n < 0) 
-  {
-     error("ERROR reading from socket\n");
+  if (n < 0) {
+    error("ERROR reading from socket\n");
   }
 
   printf("Here is the message\n---\n%s\n---\n", buffer);
